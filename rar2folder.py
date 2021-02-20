d463499409c34623e508523276842f3dd5540077
svtgeorgiev <70657778+svtgeorgiev@users.noreply.github.com>
2021-02-20 14:44:52 +0200

    Add files via upload

--- rar2folder.py
+++ rar2folder.py
@@ -15,13 +15,10 @@ temp_dir_list = []
 for (dirpath, dirnames, filenames) in walk(root):
     root_file_list.extend(filenames)
     break
-print(f"lista s failovete w osnownata direktoria {root_file_list}")
 
 for year_string in root_file_list:
     year_list.append(year_string[0:4])
-year_list = list(dict.fromkeys(year_list))
-
-print(f"lista s poddirektorii-godini {year_list}")
+year_list = list(set(year_list))
 
 for item in year_list:
     os.mkdir(os.path.join(fpath, item))
@@ -35,14 +32,12 @@ for item in year_list:
 
 for item in year_list:
     year_folder_path = pathlib.Path(f"{root}\{item}")
-    print(f"putia do vsiaka poddirektoria-godina {year_folder_path}")
     for (dirpath, dirnames, filenames) in walk(f"{year_folder_path}"):
         year_file_list.extend(filenames)
-        print(year_file_list)
-    for month_string in year_file_list:
-        month_list.append(month_string[4:6])
-    month_list = list(dict.fromkeys(month_list))
-    print(f"list s imenata na mesechnite direktorii {month_list}")
+        for month_string in year_file_list:
+            month_list.append(month_string[4:6])
+        month_list = list(set(month_list))
+        year_file_list.clear()
     for month in month_list:
         os.mkdir(os.path.join(f"{year_folder_path}\{month}"))
-        month_list.clear()
+    month_list.clear()
